bc77145213a36a2df844920cc50edc62d7a4bb2e
v_maishuren <maisrcn@qq.com>
2022-05-23 17:46:33 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # oauth2-authentication-center
 基于OAuth2的统一认证中心
+
+前端：frontend-vue-admin，基于[vue-element-admin](https://github.com/PanJiaChen/vue-element-admin)
+
+后端：unified-auth-center
+
+Spring Boot 2.6.8
+
+JDK11
+
+**TODO**
+
---
+++ unified-auth-center/src/main/java/org/msr/unifiedauthcenter/UnifiedAuthCenterApplication.java
@@ -0,0 +1,13 @@
+package org.msr.unifiedauthcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifiedAuthCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnifiedAuthCenterApplication.class, args);
+    }
+
+}
